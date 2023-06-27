f41b053b69db304a57f5ce91f6025dabe5b5fbbb
Sarah Reimann <reimannsarah45@gmail.com>
2023-06-27 16:18:28 -0700

    add dancing lettuce
    co-authored-by: Emma Gerigscott <elgerig@gmail.com>

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import DayControl from './DayControl';
 import Header from './Header';
 import SeasonControl from './SeasonControl';
+import lettuce from './../img/lettuce4real.gif';
 
 function App() {
   return (
     <React.Fragment>
       <Header />
+      <img src={lettuce} alt="dancing lettuce" className="lettuce"/>
       <div className="main">
         <div className="day result">
           <DayControl />
